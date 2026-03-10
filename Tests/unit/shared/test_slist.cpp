9f7fcaabd7f99145537fd19780e8be103986168f
slemanz <william_sleman@hotmail.com>
2026-03-10 05:19:13 -0300

    tests(feat): finish slist

--- Tests/unit/shared/test_slist.cpp
+++ Tests/unit/shared/test_slist.cpp
@@ -107,3 +107,88 @@ TEST(SList, RemoveMiddle)
     /* Verifica que a -> c */
     POINTERS_EQUAL(&c.node, a.node.next);
 }
+
+TEST(SList, RemoveTail)
+{
+    MAKE_ITEM(a, 1);
+    MAKE_ITEM(b, 2);
+
+    slist_push_back(&list, &a.node);
+    slist_push_back(&list, &b.node);
+
+    slist_remove(&list, &b.node);
+
+    UNSIGNED_LONGS_EQUAL(1, slist_count(&list));
+    POINTERS_EQUAL(NULL, a.node.next);
+}
+
+TEST(SList, RemoveOnlyElement)
+{
+    MAKE_ITEM(a, 1);
+    slist_push_front(&list, &a.node);
+    slist_remove(&list, &a.node);
+
+    CHECK(slist_empty(&list));
+    UNSIGNED_LONGS_EQUAL(0, slist_count(&list));
+}
+
+TEST(SList, RemoveNonExistentDoesNothing)
+{
+    MAKE_ITEM(a, 1);
+    MAKE_ITEM(b, 2);
+
+    slist_push_front(&list, &a.node);
+    slist_remove(&list, &b.node);  /* b não está na lista */
+
+    UNSIGNED_LONGS_EQUAL(1, slist_count(&list));
+}
+
+/* ---- Contains ---------------------------------------------------- */
+
+TEST(SList, ContainsFindsInsertedNode)
+{
+    MAKE_ITEM(a, 1);
+    slist_push_front(&list, &a.node);
+    CHECK(slist_contains(&list, &a.node));
+}
+
+TEST(SList, ContainsReturnsFalseForMissing)
+{
+    MAKE_ITEM(a, 1);
+    CHECK_FALSE(slist_contains(&list, &a.node));
+}
+
+/* ---- Iteration with container_of --------------------------------- */
+
+TEST(SList, ForEachVisitsAllElements)
+{
+    MAKE_ITEM(a, 10);
+    MAKE_ITEM(b, 20);
+    MAKE_ITEM(c, 30);
+
+    slist_push_back(&list, &a.node);
+    slist_push_back(&list, &b.node);
+    slist_push_back(&list, &c.node);
+
+    uint32_t sum = 0;
+    slist_node_t *it;
+    slist_for_each(it, &list)
+    {
+        item_t *item = container_of(it, item_t, node);
+        sum += item->id;
+    }
+
+    UNSIGNED_LONGS_EQUAL(60, sum);
+}
+
+TEST(SList, ForEachOnEmptyListDoesNothing)
+{
+    uint32_t count = 0;
+    slist_node_t *it;
+    slist_for_each(it, &list)
+    {
+        count++;
+    }
+
+    UNSIGNED_LONGS_EQUAL(0, count);
+}
